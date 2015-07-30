7f0cab7dd41ea17655ddff3d925c440577511c5c
Abhi Gupta <abhigupta99@icloud.com>
2015-07-30 15:43:02 -0400

    Added games

---
+++ src/lines.py
@@ -0,0 +1,16 @@
+import glob
+
+countLines = 0
+countWords = 0
+files = glob.glob("*.py")
+files.remove("lines.py")
+
+for x in range(len(files)):
+	fileLine = open(files[x]).read().split("\n")
+	countLines += len(fileLine)
+	for word in fileLine:
+		word = word.split()
+		countWords += len(word)
+
+print("Lines: %d" % countLines)
+print("Words: %d" % countWords)
