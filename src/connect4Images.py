86b66a1465f00aaeebc08cb3ce4a35251d2b8b85
Abhi Gupta <abhigupta99@icloud.com>
2015-07-30 15:46:36 -0400

    Deleted files

--- src/connect4Images.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'Masoud Harati'
-
-from pygame import *
-
-background = image.load("imgs/connect4Imgs/background.jpg") # Background Image for the Game
-button = image.load("imgs/connect4Imgs/button.png")
-board = image.load("imgs/connect4Imgs/board.jpg")
