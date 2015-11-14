cc2eee8e3e36e4b5146f1459b0afcde6a5d8ae47
MasoudH <masoudharati96@gmail.com>
2015-11-13 21:16:18 -0500

    Erased files due to credit issue

--- src/connect4Images.py
+++
@@ -1,5 +0,0 @@
-from pygame import *
-
-background = image.load("imgs/connect4Imgs/background.jpg") # Background Image for the Game
-button = image.load("imgs/connect4Imgs/button.png")
-board = image.load("imgs/connect4Imgs/board.jpg")
