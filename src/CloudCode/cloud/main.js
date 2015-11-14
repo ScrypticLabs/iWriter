59555040b3006c085917de76f50212c2768edb8d
MasoudH <masoudharati96@gmail.com>
2015-11-13 21:10:26 -0500

    New Updates:

--- src/CloudCode/cloud/main.js
+++ src/CloudCode/cloud/main.js
@@ -1,8 +1,9 @@
-// -*- coding: utf-8 -*-
-// @Author: Abhi
-// @Date:   2015-06-11 14:34:24
-// @Last Modified by:   Abhi Gupta
-// @Last Modified time: 2015-06-11 17:36:27
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+// Parse.Cloud.define("hello", function(request, response) {
+//   response.success("Hello world!");
+// });
 
 var twilio = require("twilio");
 twilio.initialize("AC0bec7092250cd3794b159a91b9dd1074","1569df3a5048cdcdbb2779b8b45924f1");
